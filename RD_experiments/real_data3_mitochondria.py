d14ac63db94f578342041bbbd9e7be278c9eee12
Xin <59591255+CsnowyLstar@users.noreply.github.com>
2025-06-11 18:56:58 +0800

    Add files via upload

---
+++ RD_experiments/real_data3_mitochondria.py
@@ -0,0 +1,114 @@
+import numpy as np
+import random as random
+import torch
+import torch.nn as nn 
+import pandas as pd
+import torchdiffeq as ode
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import random
+from scipy import sparse
+from utils.cubic_spline import CSpline
+import argparse
+from scipy import signal
+from utils.RC_EWS import RC_EWM
+from utils.Baseline_EWS import BL_EWS
+
+################################################################
+###  (1) Hyperparameter setting                              ###
+################################################################
+def args(): 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('--n', type=int, default=40)
+    parser.add_argument('--connectivity', type=float, default=0.05)
+    parser.add_argument('--spectral_radius', type=float, default=0.95)
+    parser.add_argument('--input_scaling', type=float, default=0.1)
+    parser.add_argument('--leak', type=float, default=0.0)
+    parser.add_argument('--b', type=float, default=0.65)
+    parser.add_argument('--alpha', type=float, default=1e-5)
+    parser.add_argument('--warm_up', type=int, default=20)
+    parser.add_argument('--method', type=str, default='euler') 
+    args = parser.parse_args(args=[])
+    return(args)
+
+args = args()
+seed = 0
+np.random.seed(seed)
+random.seed(seed)
+
+
+################################################################
+###  (2) Read data                                           ###
+################################################################
+data = np.loadtxt('real_data/mitochondria.txt', delimiter='\t')
+ts = data[:,0]
+ts = ts - ts[0]
+X = data[:,1][:,None]
+dt = (ts[-1]-ts[0])/(len(ts)-1)
+tpoints = len(ts)
+
+
+################################################################
+###  (3) Calculate the RC EWS                                ###
+################################################################
+window = 100
+step = 3
+index = 'max_eigenvalue' # select from ['max_eigenvalue','max_floquet','max_lyapunov']
+
+num = 1
+ns = [50]
+DEJ_diss = []; tm_diss = []
+for ni in range(num):
+    iscontinuous = False
+    RCDI_dis = RC_EWM(X, ts, window, step, args, iscontinuous)
+    DEJ_dis, tm_dis = RCDI_dis.calculate(index)
+    DEJ_diss.append(DEJ_dis); tm_diss.append(tm_dis)
+
+
+################################################################
+###  (4) Draw and save                                       ###
+################################################################
+fig = plt.figure(figsize=(20,18))
+ls = 40; ms = 15; tp = 2.74
+ax1 = fig.add_subplot(3,1,1)
+ax1.plot(ts,X,'b')
+ax1.tick_params(labelsize=ls)
+ax1.tick_params(axis='y', colors='b')
+ax1.set_ylabel(r"$s$",size=ls,color='b')
+ax1.set_xticks([])
+ax1.set_xlim(ts[0],ts[-1])
+
+ax3 = fig.add_subplot(3,1,2)
+ind = 0
+isrealpart = False
+for ni in range(num):
+    tm_dis = tm_diss[ni]; DEJ_dis = DEJ_diss[ni]
+    i = next((i for i in range(len(tm_dis)) if tm_dis[i] > tp), None)
+    DEJ_di = DEJ_dis[:i,0] if isrealpart else np.sqrt(DEJ_dis[:i,0]**2 + DEJ_dis[:i,1]**2)
+    ax3.plot(tm_dis[:i],DEJ_di,'m*',markersize=ms)
+    if ind == 0:
+        xs = tm_dis[:i]; ys=DEJ_di; ind = 1
+    else:
+        xs = np.concatenate((xs,tm_dis[:i]),axis=0)
+        ys = np.concatenate((ys,DEJ_di),axis=0)
+coefficients = np.polyfit(xs, ys, 1)
+poly_func = np.poly1d(coefficients)
+ax3.plot([tm_dis[0],tm_dis[i]],[poly_func(tm_dis[0]),poly_func(tm_dis[i])],'r-',linewidth=8,alpha=0.6)
+ax3.tick_params(labelsize=ls)
+ax3.tick_params(axis='y', colors='m')
+ax3.set_ylabel("RCDI",size=ls,color='m')
+ax3.set_xlim(ts[0],ts[-1])
+ax3.set_xlabel('Time',size=ls)
+
+moln = 'real_data3'
+X_pd = pd.DataFrame(X)
+index_pd = pd.DataFrame(np.sqrt(DEJ_dis[:,0]**2 + DEJ_dis[:,1]**2))
+ts_pd = pd.DataFrame(ts)
+tm_pd = pd.DataFrame(tm_dis)
+X_pd.to_csv('results/X_'+moln+'_RCDI_.csv')
+index_pd.to_csv('results/index_'+moln+'_RCDI_.csv')
+ts_pd.to_csv('results/ts_'+moln+'_RCDI_.csv')
+tm_pd.to_csv('results/tm_'+moln+'_RCDI_.csv')
+
+plt.savefig("results/r3.pdf")
+
---
+++ RD_experiments/real_data4_thermoacoustic.py
@@ -0,0 +1,142 @@
+import numpy as np
+import random as random
+import torch
+import torch.nn as nn 
+import pandas as pd
+import torchdiffeq as ode
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import random
+from scipy import sparse
+from utils.cubic_spline import CSpline
+import argparse
+from scipy import signal
+from utils.RC_EWS import RC_EWM
+#from utils.Baseline_EWS import BL_EWS
+
+################################################################
+###  (1) Hyperparameter setting                              ###
+################################################################
+def args(): 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('--n', type=int, default=300)
+    parser.add_argument('--connectivity', type=float, default=0.05)
+    parser.add_argument('--spectral_radius', type=float, default=1.25)
+    parser.add_argument('--input_scaling', type=float, default=1.0)
+    parser.add_argument('--leak', type=float, default=0.0)
+    parser.add_argument('--b', type=float, default=1.0)
+    parser.add_argument('--alpha', type=float, default=1e-5)
+    parser.add_argument('--warm_up', type=int, default=5000)
+    parser.add_argument('--method', type=str, default='euler') 
+    args = parser.parse_args(args=[])
+    return(args)
+
+args = args()
+seed = 0
+np.random.seed(seed)
+random.seed(seed)
+
+
+################################################################
+###  (2) Read data                                           ###
+################################################################
+data = np.loadtxt('real_data/thermoacoustic.txt', delimiter='\t')[120000:,1]
+X = data[:,None]
+ts = np.linspace(0,8,X.shape[0])
+dt = ts[-1]/(len(ts)-1)
+tpoints = len(ts)
+
+
+################################################################
+###  (3) Calculate the RC EWS                                ###
+################################################################
+window = 2000
+step = 400
+index = 'max_eigenvalue' # select from ['max_eigenvalue','max_floquet','max_lyapunov']
+
+num = 1
+ns = [300,300,300]
+DEJ_cons = []; tm_cons = [] 
+DEJ_diss = []; tm_diss = []
+for ni in range(num):
+    args.n = ns[ni]
+    iscontinuous = True
+    isdetrend = False
+    RCDI_con = RC_EWM(X, ts, window, step, args, iscontinuous, isdetrend)
+    DEJ_con, tm_con = RCDI_con.calculate(index)
+    DEJ_cons.append(DEJ_con); tm_cons.append(tm_con)
+    
+    iscontinuous = False
+    isdetrend = False
+    RCDI_dis = RC_EWM(X, ts, window, step, args, iscontinuous, isdetrend)
+    DEJ_dis, tm_dis = RCDI_dis.calculate(index)
+    DEJ_diss.append(DEJ_dis); tm_diss.append(tm_dis)
+
+
+################################################################
+###  (5) Draw and save                                       ###
+################################################################
+fig = plt.figure(figsize=(20,18))
+ls = 40; ms = 15; tp = 6.2
+ax1 = fig.add_subplot(3,1,1)
+ax1.plot(ts,X,'b')
+#ax1.plot(ts[::10],X[::10],'b')
+ax1.tick_params(labelsize=ls)
+ax1.tick_params(axis='y', colors='b')
+ax1.set_ylabel(r"$s$",size=ls,color='b')
+ax1.set_xticks([])
+ax1.set_xlim(ts[0],ts[-1])
+
+ax2 = fig.add_subplot(3,1,2)
+ind = 0
+for ni in range(num):
+    tm_con = tm_cons[ni]; DEJ_con = DEJ_cons[ni]
+    i = next((i for i in range(len(tm_con)) if tm_con[i] > tp), None)
+    ax2.plot(tm_con[:i],DEJ_con[:i,0],'r*',markersize=ms)
+    if ind == 0:
+        xs = tm_con[:i]; ys=DEJ_con[:i,0]; ind = 1
+    else:
+        xs = np.concatenate((xs,tm_con[:i]),axis=0)
+        ys = np.concatenate((ys,DEJ_con[:i,0]),axis=0)
+coefficients = np.polyfit(xs, ys, 1)
+poly_func = np.poly1d(coefficients)
+ax2.plot([tm_con[0],tm_con[i]],[poly_func(tm_con[0]),poly_func(tm_con[i])],'r-',linewidth=5)
+ax2.tick_params(labelsize=ls)
+ax2.tick_params(axis='y', colors='red')
+ax2.set_ylabel("RCDI_con",size=ls,color='red')
+ax2.set_xticks([])
+ax2.set_xlim(ts[0],ts[-1])
+
+ax3 = fig.add_subplot(3,1,3)
+ind = 0
+isrealpart = False
+for ni in range(num):
+    tm_dis = tm_diss[ni]; DEJ_dis = DEJ_diss[ni]
+    i = next((i for i in range(len(tm_dis)) if tm_dis[i] > tp), None)
+    DEJ_di = DEJ_dis[:i,0] if isrealpart else np.sqrt(DEJ_dis[:i,0]**2 + DEJ_dis[:i,1]**2)
+    ax3.plot(tm_dis[:i],DEJ_di,'m*',markersize=ms)
+    if ind == 0:
+        xs = tm_dis[:i]; ys=DEJ_di; ind = 1
+    else:
+        xs = np.concatenate((xs,tm_dis[:i]),axis=0)
+        ys = np.concatenate((ys,DEJ_di),axis=0)
+coefficients = np.polyfit(xs, ys, 1)
+poly_func = np.poly1d(coefficients)
+ax3.plot([tm_dis[0],tm_dis[i]],[poly_func(tm_dis[0]),poly_func(tm_dis[i])],'m-',linewidth=5)
+ax3.tick_params(labelsize=ls)
+ax3.tick_params(axis='y', colors='m')
+ax3.set_ylabel("RCDI_dis",size=ls,color='m')
+ax3.set_xlim(ts[0],ts[-1])
+ax3.set_xlabel('Time',size=ls)
+
+
+moln = 'real_data4'
+X_pd = pd.DataFrame(X)
+index_pd = pd.DataFrame(np.sqrt(DEJ_dis[:,0]**2 + DEJ_dis[:,1]**2))
+ts_pd = pd.DataFrame(ts)
+tm_pd = pd.DataFrame(tm_dis)
+X_pd.to_csv('results/X_'+moln+'_RCDI_.csv')
+index_pd.to_csv('results/index_'+moln+'_RCDI_.csv')
+ts_pd.to_csv('results/ts_'+moln+'_RCDI_.csv')
+tm_pd.to_csv('results/tm_'+moln+'_RCDI_.csv')
+

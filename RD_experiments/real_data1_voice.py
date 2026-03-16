3617035b11f80a1001745edd0a5daa3c272d02ad
Xin <59591255+CsnowyLstar@users.noreply.github.com>
2026-03-16 20:11:17 +0800

    Update real_data1_voice.py

--- RD_experiments/real_data1_voice.py
+++ RD_experiments/real_data1_voice.py
@@ -1,4 +1,72 @@
+import numpy as np
+import random as random
+import torch
+import torch.nn as nn 
+import pandas as pd
+import torchdiffeq as ode
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import random
+from scipy import sparse
+from utils.cubic_spline import CSpline
+import argparse
+from scipy import signal
+from utils.RC_EWS import RC_EWM
+from utils.Baseline_EWS import BL_EWS
 
+################################################################
+###  (1) Hyperparameter setting                              ###
+################################################################
+def args(): 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('--n', type=int, default=500)
+    parser.add_argument('--connectivity', type=float, default=0.05)
+    parser.add_argument('--spectral_radius', type=float, default=0.95)
+    parser.add_argument('--input_scaling', type=float, default=0.1)
+    parser.add_argument('--leak', type=float, default=0.0)
+    parser.add_argument('--b', type=float, default=1.0)
+    parser.add_argument('--alpha', type=float, default=1e-5)
+    parser.add_argument('--warm_up', type=int, default=800)
+    parser.add_argument('--method', type=str, default='euler') 
+    args = parser.parse_args(args=[])
+    return(args)
+
+args = args()
+seed = 0
+np.random.seed(seed)
+random.seed(seed)
+
+
+################################################################
+###  (2) Read data                                           ###
+################################################################
+data = np.loadtxt('real_data/voice.txt', delimiter='\t')
+ts = data[:,0]-data[0,0]
+X = data[:,1][:,None]
+dt = ts[-1]/(len(ts)-1)
+tpoints = len(ts)
+tp = 0.23
+
+print("X.shape:",X.shape)
+
+
+################################################################
+###  (3) Calculate the RC EWS                                ###
+################################################################
+window = 1000
+step = 100
+index = 'max_eigenvalue' # select from ['max_eigenvalue','max_floquet','max_lyapunov']
+
+num = 1
+ns = [400]
+DEJ_diss = []; tm_diss = []
+for ni in range(num):
+    args.n = ns[ni]
+    iscontinuous = False
+    RCDyM_dis = RC_EWM(X, ts, window, step, args, iscontinuous)
+    DEJ_dis, tm_dis = RCDyM_dis.calculate(index)
+    DEJ_diss.append(DEJ_dis); tm_diss.append(tm_dis)
+    
 ################################################################
 ###  (4) Refined low-order polynomial                        ###
 ################################################################
@@ -33,7 +101,6 @@ def monotonic_polyfit_nonnegative(x_data, y_data, degree, alpha=0.1):
         return coeffs_init
 
     return result.x
-    
 
 
 ################################################################
@@ -92,5 +159,4 @@ ts_pd.to_csv('results/ts_'+moln+'_RCDyM_.csv')
 tm_pd.to_csv('results/tm_'+moln+'_RCDyM_.csv')
 
 plt.savefig("results/r1.png")
-
 
